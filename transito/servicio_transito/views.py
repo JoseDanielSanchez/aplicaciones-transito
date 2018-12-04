abecadfeb3cc73f2eeaf44ab3b2eaf49a7343319
JoseDanielSanchez <jose.sanchezcastillo@ucr.ac.cr>
2018-12-03 22:30:07 -0600

    Update views.py

--- transito/servicio_transito/views.py
+++ transito/servicio_transito/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from servicio_transito.serializers import UserSerializer, GroupSerializer
+from transito.servicio_transito.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
