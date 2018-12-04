7f7f22eb020e4ed119632439c08aafe55afcd02e
JoseDanielSanchez <jose.sanchezcastillo@ucr.ac.cr>
2018-12-03 22:24:52 -0600

    Update urls.py

--- transito/transito/urls.py
+++ transito/transito/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 from rest_framework import routers
-from servicio_transito import views
+from transito.servicio_transito import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
