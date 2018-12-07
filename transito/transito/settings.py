7adf19bbeb638209cd4f8881914e4a47aa30a8a1
JoseDanielSanchez <jose.sanchezcastillo@ucr.ac.cr>
2018-12-06 21:45:56 -0600

    Update settings.py

--- transito/transito/settings.py
+++ transito/transito/settings.py
@@ -144,4 +144,25 @@ REST_FRAMEWORK = {
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-CORS_ORIGIN_WHITELIST ='localhost'
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
